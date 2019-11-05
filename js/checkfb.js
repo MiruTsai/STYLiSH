350411ada08249772a785b92c57a4fd7905abe0c
MiruTsai <zct398@gmail.com>
2019-11-05 12:53:42 +0800

    提出 FB sdk

--- js/checkfb.js
+++ js/checkfb.js
@@ -4,6 +4,32 @@ const text2 = document.querySelector(".content2");
 const member = document.querySelector(".btn_member01_normal");
 let token;
 
+let fb={};
+
+fb.load=function(){
+	// Load the SDK asynchronously
+	(function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, "script", "facebook-jssdk"));
+fb.init=function(){
+	FB.init({
+		appId: "2888776061346906",
+                cookie: true,
+                xfbml: true,
+                version: "v3.3"
+	});
+	FB.getLoginStatus(function(response){
+		app.fb.loginStatusChange(response);
+	});
+};
+}
+window.fbAsyncInit=fb.init;
+
+
 //創建API所需body
 let parseToken;
 let FBtoken;
